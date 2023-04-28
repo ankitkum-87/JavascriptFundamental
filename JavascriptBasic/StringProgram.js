af06ed5c945709a1ce7383e7eade065443875307
ankitkum-87 <62784726+ankitkum-87@users.noreply.github.com>
2023-04-28 05:30:51 +0000

    String Reverse program

---
+++ JavascriptBasic/StringProgram.js
@@ -0,0 +1,10 @@
+// Program to reverse the all words in  a given string
+let str="welcome to tcs";
+let emptyStr="";
+for(let x of str){
+    // emptyStr= x + emptyStr.trim();// for remove the space 
+    emptyStr= x + emptyStr;
+    //console.log(emptyStr);
+
+}
+console.log("Final Output is :- ",emptyStr);
